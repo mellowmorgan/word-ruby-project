c586927c7386692c43fda812755bdb20c26c2d5f
mellowmorgan <morgan.waites@gmail.com>
2021-12-17 10:40:49 -0800

    WIP: working on get post routes for homepage

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/stage')
-require('./lib/artist')
+require('./lib/definition')
+require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:home)
+end
+
+get('/home') do
+  erb(:home)
+end
+
+post('/home') do
+  word = Word.new(:term => params[:word], :id => nil)
+  word.save
+  erb(:home)
+end
