9137c27c37373d498b0fbd44f99e31b10174acc9
mellowmorgan <morgan.waites@gmail.com>
2021-12-17 09:35:22 -0800

    clear and save and all methods added to Definition class and tests passed

--- lib/Definition.rb
+++ lib/Definition.rb
@@ -3,10 +3,22 @@ require("pry")
 class Definition
   attr_accessor :def, :id
   @@total_rows = 0
-
+  @@definitions = {}
   def initialize(attributes)
     @def = attributes.fetch(:def)
     @id = attributes.fetch(:id) || @@total_rows += 1
   end
-
+  def self.clear
+    @@total_rows = 0
+    @@definitions = {}
+  end
+  def ==(definition_to_compare)
+    (self.id == definition_to_compare.id) && (self.def == definition_to_compare.def)
+  end
+  def save
+    @@definitions[self.id] = Definition.new({:def => self.def, :id => self.id})
+  end
+  def self.all
+    @@definitions.values
+  end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,11 +3,19 @@ require 'definition'
 
 describe('#Definition') do
   before(:each) do
+    Definition.clear
     @definition = Definition.new({:def => "to be awe-inspiring", :id => nil})
+    @definition.save
   end
   describe('#initialize') do
     it('should create Definiton class object with unique id and def attribute') do
       expect(@definition.def).to(eq('to be awe-inspiring'))
     end
   end
+  describe('#save') do
+    it('should save definition to definitions and have that one object in array when all is called') do
+      expect(Definition.all).to(eq([@definition]))
+    end
+  end
+
 end
