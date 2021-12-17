ece8932e6ce9c3db0e53959bfe1a31eca10e2c8f
mellowmorgan <morgan.waites@gmail.com>
2021-12-17 08:37:21 -0800

    add word initialize method test passed

--- lib/Word.rb
+++ lib/Word.rb
@@ -1,2 +1,15 @@
 require("pry")
+
 class Word
+  attr_accessor :term, :id
+  @@total_rows=0
+  def initialize(attributes)
+    @term = attributes.fetch(:term)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def ==(word_to_compare)
+    (self.term == word_to_compare.term) && (self.id == word_to_compare.id)
+  end
+
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,4 +1,14 @@
 require 'rspec'
-require 'artist'
-require 'stage'
 require 'pry'
+require 'word'
+
+describe('#Word') do 
+  before(:each) do
+    @word = Word.new({:term => "luminous", :id => nil})
+  end
+  describe('initialize') do
+    it('should create Word class object with unique id and term attribute luminous') do
+      expect(@word.term).to(eq('luminous'))
+    end
+  end
+end
