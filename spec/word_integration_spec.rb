4a152ccfa700123650d7749a6d01cd5cb8574c1b
mellowmorgan <morgan.waites@gmail.com>
2021-12-22 17:09:35 -0800

    add definition delete integration spec

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -28,45 +28,45 @@ describe('delete a word', {:type => :feature}) do
     click_on("ruby")
     click_on("Edit/Delete Word")
     click_on("Delete")
-    expect(page).to have_content('ruby')
+    expect(page).to have_no_content('ruby')
   end
 end 
 
-# describe('update word', {:type => :feature}) do
-#   it('update ruby to red') do
-#     visit('/home')
-#     fill_in('word', :with => 'ruby')
-#     click_on("Add Word")
-#     click_on("ruby")
-#     click_on("Edit/Delete Word")
-#     fill_in("new_term", :with => "red")
-#     click_on("Update")
-#     expect(page).to have_content('red')
-#   end
-# end 
+describe('update word', {:type => :feature}) do
+  it('update ruby to red') do
+    visit('/home')
+    fill_in('word', :with => 'ruby')
+    click_on("Add Word")
+    click_on("ruby")
+    click_on("Edit/Delete Word")
+    fill_in("new_term", :with => "red")
+    click_on("Update")
+    expect(page).to have_content('red')
+  end
+end 
 
-# describe('update definition', {:type => :feature}) do
-#   it('update definition of red') do
-#     visit('/home')
-#     click_on("red")
-#     fill_in("definition", :with => "an object-oriented programming language")
-#     click_on("Add Definition")
-#     click_on("an object-oriented programming language")
-#     fill_in("new_def", :with => "the color of blood")
-#     click_on("Update")
-#     expect(page).to have_content('blood')
-#   end
-# end 
+describe('update definition', {:type => :feature}) do
+  it('update definition of red') do
+    visit('/home')
+    click_on("red")
+    fill_in("definition", :with => "an object-oriented programming language")
+    click_on("Add Definition")
+    click_on("an object-oriented programming language")
+    fill_in("new_def", :with => "the color of blood")
+    click_on("Update")
+    expect(page).to have_content('blood')
+  end
+end 
 
-# describe('delete definition', {:type => :feature}) do
-#   it('update definition of red') do
-#     visit('/home')
-#     click_on("red")
-#     click_on("the color of blood")
-#     click_on("Delete")
-#     expect(page).to have_no_content('blood')
-#   end
-# end 
+describe('delete definition', {:type => :feature}) do
+  it('delete definition color of blood') do
+    visit('/home')
+    click_on("red")
+    click_on("the color of blood")
+    click_on("Delete")
+    expect(page).to have_no_content('blood')
+  end
+end 
 
 
 
